c26df5b9b1c342debc562dfad48378c5b62126c1
MarcEhrlich <165007370+MarcEhrlich@users.noreply.github.com>
2024-11-25 12:57:55 +0100

    Update

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+// global: man kann auch von aussen zugreifen
+var variable1 = 1;
+
+// ist local verfügbar und konstant
+const konstante2 = 2;
+
+// ist local verfügbar und veränderbar
+//let variable3 = 3;
+
+if (true) {
+  let  variable3 = 9;
+}
+
+console.log(variable3);
